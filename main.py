e724824ff11a947d34659297d52ee39b77e15971
Sebastian Granda Gallego <sgg10.develop@gmail.com>
2022-03-06 20:55:13 -0500

    Init Flask app

---
+++ main.py
@@ -0,0 +1,28 @@
+from app import create_app
+
+app = create_app()
+
+
+@app.route("/")
+def index():
+    return {
+        "message": "Welcome to ARSP Solver API"
+    }
+
+# Error Handlers
+
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return {
+        "name": "Error 500 - Internal Server Error",
+        "message": error.description
+    }
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return {
+        "name": "Error 404 - Not Found",
+        "message": error.description
+    }
