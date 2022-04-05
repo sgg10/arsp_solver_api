667ef51b59b9b1dcc8e04a00c033c6f07f00f5af
Pascual Gomez L <pascualgomezl@MacBook-Pro-de-Pascual.local>
2022-04-05 14:23:17 -0500

    Gaussian tridiagonal elimination improved

--- MATLAB/A5_gaustridiagonal.m
+++ MATLAB/A5_gaustridiagonal.m
@@ -14,11 +14,18 @@
 function [x,M] = A5_gaustridiagonal(A,b) 
 
 %Inicialización
-n=size(A,1);
+N=size(A,1);
 M=[A b];
 
+d = b;
+a = diag(A,-1);
+b = diag(A);
+c = diag(A,1);
 
-fileID = fopen('sol_gauss.txt','wt'); % Crea un archivo .txt
+c(1) = c(1) / b(1);
+d(1) = d(1) / b(1); 
+
+fileID = fopen('sol_gausstri.txt','wt'); % Crea un archivo .txt
 fprintf(fileID,'%s\r\n',"Matriz inicial"); % Genera la primera línea
 fprintf(fileID,'%s\r\n',"---------------");
 %NOTA: \r\n sirve como salto de línea para archivos .txt de windows.
@@ -28,39 +35,25 @@ for ii = 1:length(M(:,1))
     fprintf(fileID,'\n'); % Genera un salto de línea
 end
 
-
-
-%Reducimos el sistema
-for i=1:n-1
-    for j=i+1:n
-        if M(j,i)~=0
-           M(j,i:n+1)=M(j,i:n+1)-(M(j,i)/M(i,i))*M(i,i:n+1);
-        else
-            fprintf(fileID,'%s\r\n',"---------------");
-            fprintf(fileID,'%s\r\n',"ERROR: La diagonal tiene un cero."); % Imprime el número de la etapa
-            fprintf(fileID,'%s\r\n',"---------------");
-        end
+for n = 2:1:N-1
+    temp = b(n) - a(n) * c(n - 1);
+    if (n<N)
+        c(n) = c(n) / temp;
     end
-
-    fprintf(fileID,'%s\r\n',"---------------");
-    fprintf(fileID,'%s\r\n',"Etapa " + i); % Imprime el número de la etapa
-    fprintf(fileID,'%s\r\n',"---------------");
-    for ii = 1:length(M(:,1))
-    fprintf(fileID,'%.5f %.7d %g\t',M(ii,:)); % Imprime cada fila de la matriz
-    fprintf(fileID,'\n'); % Genera un salto de línea
-    end
-
+    d(n) = (d(n) - a(n) * d(n - 1)) / temp;
+end
+ 
+x(N) = d(N);
+for n = (N - 1):-1:1
+    x(n) = d(n) - c(n) * x(n + 1);
 end
 
-%Entrega de resultados
-x=sustregr(M); %Sustitución regresiva
-
 fprintf(fileID,'%s\r\n',"---------------");
-fprintf(fileID,'%s\r\n',"Solución"); % Imprime el número de la etapa
+fprintf(fileID,'%s\r\n',"Solución");
 fprintf(fileID,'%s\r\n',"---------------");
 for ii = 1:length(x(:,1))
-fprintf(fileID,'%.5f %.7d %g \t',x(ii,:)); % Imprime cada fila de la matriz
-fprintf(fileID,'\n'); % Genera un salto de línea
+    fprintf(fileID,'%.5f %.7d %g \t',x(ii,:)); % Imprime cada fila de la matriz
+    fprintf(fileID,'\n'); % Genera un salto de línea
 end
 
 fclose(fileID);
