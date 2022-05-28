9a985e269811f2a22b51d90d7a8cc408f804c395
Alejandro Rodriguez Muñoz <arodriguem@eafit.edu.co>
2022-05-28 11:53:50 -0500

    Fix Partial LU

--- app/linear_equations/factorization/partial_lu.py
+++ app/linear_equations/factorization/partial_lu.py
@@ -1,16 +1,14 @@
-#from app.utils.methods import BaseMethod
+from app.utils.methods import BaseMethod
 import scipy
 import scipy.linalg
 import numpy as np
 
-class PartialLU():
+class PartialLU(BaseMethod):
     def __init__(self, A):
         self.P, self.L, self.U = scipy.linalg.lu(A)
-
     def run(self):
         
         return {"result": {"L": self.L, "U": self.U, "P": np.transpose(self.P)}}
-
 
 if __name__ == "__main__":
     A = [[2,4,3,-2],[4,9,7,-3],[-2,-3,2,7]]
