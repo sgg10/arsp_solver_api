2966328343a09f374d770067b473a96b2d05022f
Alejandro Rodriguez Muñoz <arodriguem@eafit.edu.co>
2022-05-28 14:20:37 -0500

    Fixed neeton divided differences

--- app/interpolation/methods/newton_divided_differences.py
+++ app/interpolation/methods/newton_divided_differences.py
@@ -1,37 +1,56 @@
 from app.utils.methods import BaseMethod
+from audioop import reverse
+from typing import List
 from sympy import Function, expand
 from sympy.parsing.sympy_parser import parse_expr
+import numpy as np
+
 
 
 class NewtonDifDiv(BaseMethod):
-    def __init__(self, n, table):
-        self.n = int(n)
-        self.tabla = table
+
+    def __init__(self, x, y ):
+        self.x = np.array(x)
+        self.y = np.array(y)
+        
+
+    def divided_diff(self, x, y):
+        '''
+        function to calculate the divided
+        differences table
+        '''
+        n = x.size
+        q = np.zeros((n, n - 1))
+
+        # Insert 'y' in the first column of the matrix 'q'
+        q = np.concatenate((y[:, None], q), axis=1)
+
+        for i in range(1, n):
+            for j in range(1, i + 1):
+                q[i, j] = (q[i, j - 1] - q[i - 1, j - 1]) / (x[i] - x[i - j])
+
+        # Copy the diagonal values of the matrix q to the vector f
+        f = np.zeros(n)
+        for i in range(0, n):
+            f[i] = q[i, i]
+
+        # Prints the polynomial
+        pol = '{}'.format("p(x)={:+.4f}".format(f[0]), end="")
+        for i in range(1, n):
+            pol += '{}'.format("{:+.4f}".format(f[i]), end="")
+            for j in range(1, i + 1):
+                pol += '{}'.format("(x{:+.4f})".format(x[j] * -1), end="")
+
+        
+        return q,[f],pol
 
     def run(self):
-        polinimio = f'P(X) = {self.tabla[0][1]}'
-        F = Function('F')
-        for j in range(2, self.n + 1):
-            for i in range(j - 1, self.n):
-                self.tabla[i][j] = (self.tabla[i][j - 1] - self.tabla[i - 1][j - 1]) / (
-                            self.tabla[i][0] - self.tabla[i - j + 1][0])
-                if i == j - 1:
-                    polinimio += f' + {self.tabla[i][j]}'
-                    for k in range(0, i):
-                        print(f'i={i} j={j} k={k}')
-                        polinimio += f'(x - {self.tabla[k][0]})'
-        F = parse_expr(polinimio.replace('P(X) = ', '').replace('(', '*('))
-        self.printTabla(self.tabla, self.n)
-        return {
-            'result': f'P(x) = {expand(F)}',
-            'schematic_form': polinimio
-        }
+        m,coef, pol = self.divided_diff(self.x, self.y)
+        return {"result": {"Matrix":m, "Coef": coef, "Pol": pol}}
 
-    def printTabla(self, tabla, n):
-        print(
-            "n |   xi   |      f[xi]     |         primera         |          Segunda          |          Tercera     "
-            "     |         Cuarta         |         Quinta        |Nesima|")
-        for i in range(n):
-            print(str(i) + "     " + str(tabla[i]).replace("'", " ").replace(",", "       ").replace("[", " ").replace(
-                "]", " ").replace(" 0 ", " "))
-            print("\n")
+"""
+if __name__ == "__main__":
+    x=[2,3,4,5,6]
+    y=[2,6,5,5,6]
+    print(NewtonDifDiv(x, y).run())
+"""
