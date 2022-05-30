b9dab8fd0005bff0fe45f55841403b1f69821649
Alejandro Rodriguez Muñoz <arodriguem@eafit.edu.co>
2022-05-30 12:58:34 -0500

    Fixed cholesky

--- app/interpolation/methods/larange.py
+++ app/interpolation/methods/larange.py
@@ -1,43 +1,47 @@
-from app.utils.methods import BaseMethod
-from sympy import expand, Function
+#from app.utils.methods import BaseMethod
+from sympy import expand, Function, symbols, simplify
 from sympy.parsing.sympy_parser import parse_expr
+import numpy as np
 
 
-class Larange(BaseMethod):
-    def __init__(self, n, x, y, **kwargs):
-        self.n = int(n)
-        self.x = x
-        self.y = y
+class Larange():
+    def __init__(self, x, y, **keywords):
+
+        self.x = np.array(x)
+        self.y = np.array(y)
         self.proceso = []
 
-    def run(self):
-        polinomio = ""
-        F = Function('F')
-        G = Function('G')
+    def lagrange(self, x, y):
+        """Interpolates a value using the 'Lagrange polynomial'.
+        Args:
+            x: an array containing x values.
+            y: an array containing y values.
+            x_int: value to interpolate.
+        Returns:
+            y_int: interpolated value.
         """
-          Forma esquematica polinomio de lagrange
-          P(X) = L0(X)F(X0) + L1(X)F(X1) + L2(X)F(X2) + ... + LN(X)F(XN)
-        """
-        for i in range(self.n):
-            L = '('
-            for j in range(self.n):
-                if j != i:
-                    L += f'(x - {self.x[j]})'
-            L += ') / ('
-            for j in range(self.n):
-                if j != i:
-                    L += f'({self.x[i]} - {self.x[j]}'
-            L += ')'
-            L = L.replace(')(', ')*(')
-            F = parse_expr(L)
-            self.proceso.append(f'\n L{i} (x) = {L.replace("((", "(").replace("))", ")")} = {expand(F)}')
-            if i == self.n - 1:
-                polinomio += f'({expand(F)})*{self.y[i]}'
-            else:
-                polinomio += f'({expand(F)})*{self.y[i]} + '
+        n = x.size
+        x_int = symbols('x')
+        y_int = 0
 
-        G = (f'P(x) = {expand(polinomio)}')
-        print(f'''POLINOMIO INTERPOLANTE DE LAGRANGE
-          P(x) = {G}
-        ''')
-        return {'result': G}
+        for i in range(0, n):
+            p = y[i]
+            for j in range(0, n):
+                if i != j:
+                    p = p * (( x_int - x[j]) / (x[i] - x[j]))
+            y_int = y_int + p
+        
+        return y_int
+
+    def run(self):
+        p = self.lagrange(self.x, self.y)
+        p = simplify(p)
+        return {'result': {"Polinomio": p}}
+
+'''
+if __name__ == "__main__":
+    x=[2,3,4,5,6]
+    y=[2,6,5,5,6]
+    print(Larange(x, y).run())
+
+'''
