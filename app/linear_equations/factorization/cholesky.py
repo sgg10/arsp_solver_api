e7f19f29e41230a56f028198e9b80170ec79fb13
Alejandro Rodriguez Muñoz <arodriguem@eafit.edu.co>
2022-05-30 21:45:22 -0500

    Fixed SOR

--- app/linear_equations/factorization/cholesky.py
+++ app/linear_equations/factorization/cholesky.py
@@ -1,9 +1,9 @@
-from app.utils.methods import BaseMethod
+#from app.utils.methods import BaseMethod
 import scipy
 import scipy.linalg
 
 
-class Cholesky(BaseMethod):
+class Cholesky():
     def __init__(self, A, **kwargs):
         self.A = A
 
@@ -11,3 +11,13 @@ class Cholesky(BaseMethod):
         L = scipy.linalg.cholesky(self.A, lower=True)
         U = scipy.linalg.cholesky(self.A, lower=False)
         return {'result': {"L: ":L,"U: ": U}}
+
+
+if __name__ == "__main__":
+    A =[
+        [4, -1, 0, 3],
+        [1, 15.5, 3, 8],
+        [0, -1.3, -4, 1.1],
+        [14, 5, -2, 30]
+    ]
+    print(Cholesky(A).run())
